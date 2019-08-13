61ee75d271d4bc7657211ee43a251f43a09aa55f
Yanu <>
2019-08-13 14:58:32 +0800

    first commit

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd `dirname $0`
+eval $(ps -ef | grep "[0-9] python3.7 server\\.py m" | awk '{print "kill "$2}')
+ulimit -n 512000
+nohup python3.7 server.py m>> /dev/null 2>&1 &
+
---
+++ stop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+eval $(ps -ef | grep "[0-9] python3.7 server\\.py m" | awk '{print "kill -9 "$2}')
